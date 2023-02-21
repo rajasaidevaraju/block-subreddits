7459cc530c05de4b8cfb26ed32fcc7ce7e5efef4
rajasaidevaraju <rajasaidevaraju@yahoo.com>
2023-02-20 22:25:20 -0500

    Now blocks pics sub

--- Scripts/content.js
+++ Scripts/content.js
@@ -1,17 +1,33 @@
-let subReddits=document.querySelectorAll("a[data-click-id]")
-for(let i=0;i<subReddits.length;i++){
-    let attributes=subReddits[i].attributes;
-    if(attributes["data-click-id"].nodeValue=="subreddit"){
-        if(subReddits[i].text!==''){
-            if(attributes['href'].nodeValue==="/r/pics/")
-            //remove the article from the page
-            removeParent(subReddits[i],8)  
-        }
+
+let previousLength=0;
+
+// Will execute removeSub every 1 seconds 
+var intervalID = window.setInterval(removeSub, 1000);
+
+
+
+function removeSub() {
+    let subReddits=document.querySelectorAll("a[data-click-id]")
+    // execute only if there is a change in sub length
+    if(subReddits.length!=previousLength){
+        previousLength=subReddits.length;
+        console.log(previousLength);
+        for(let i=0;i<subReddits.length;i++){
         
+            let attributes=subReddits[i].attributes;
+            if(attributes["data-click-id"].nodeValue=="subreddit" && subReddits[i].text!=='' && attributes['href'].nodeValue==="/r/pics/"){
+                
+                //remove the article from the page
+                removeParent(subReddits[i],8)  
+                
+            }
+        }
     }
+    
+    
 }
 
-// number is the number of times that the element should climb to reach parent
+// number param is the number of times that the element should climb to reach parent
 function removeParent(element, number){
 
     let parent = element;
@@ -20,5 +36,6 @@ function removeParent(element, number){
             parent = parent.parentNode;
         }
     }
+    
     parent.remove();
 }
