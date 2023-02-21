75741a73d3c449cfdc79a4ee736caba1874876bb
rajasaidevaraju <rajasaidevaraju@yahoo.com>
2023-02-21 02:19:01 -0500

    blocking function working

--- popup.js
+++ popup.js
@@ -1,1 +1,12 @@
-console.log("This is a popup!")
+
+function addSub(){
+    document.getElementById("subNameDiv").classList.remove('no-display')
+    console.log("I am here")
+
+}
+
+window.onload=function(){
+
+    document.getElementById("addSub").addEventListener("click", addSub);
+
+}
